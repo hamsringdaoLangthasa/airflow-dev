2b8947efe2476699e230d3fd666239b47ab2cbcf
Hamsringdao Langthasa <hamsringdaolangthasa@Mac.local>
2025-07-11 00:06:58 +0530

    add branch operator

--- dags/simple-dag.py
+++ dags/simple-dag.py
@@ -2,7 +2,14 @@ from airflow import DAG
 import pendulum
 
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+
+def _check_accuracy():
+    accuracy = 0.85
+    if accuracy > 0.8:
+        return "accurate"
+    else:
+        return "inaccurate"
 
 with DAG(
     dag_id="simple_dag",
@@ -20,6 +27,14 @@ with DAG(
         python_callable=lambda: print("Training ML model..."),
     )
 
+    check_accuracy = BranchPythonOperator(
+        task_id="check_accuracy",
+        python_callable=_check_accuracy,
+    )
+
+    accurate = EmptyOperator(task_id="accurate")
+    inaccurate = EmptyOperator(task_id="inaccurate")
+
     end = EmptyOperator(task_id="end")
 
-    start >> training_ml >> end
+    start >> training_ml >> check_accuracy >> [ accurate, inaccurate ] >> end
